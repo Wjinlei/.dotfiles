83347220d9a59b30af51facab8fe85813da9a75d
Wjinlei <1976883731@qq.com>
2020-11-14 23:00:37 +0800

    modify albert.conf and add centaur emacs config

---
+++ tag-centaur/emacs.d/custom.el
@@ -0,0 +1,88 @@
+;;; custom.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
+;;; Commentary:
+;;;       Add or change the configurations in custom.el, then restart Emacs.
+;;;       Put your own configurations in custom-post.el to override default configurations.
+;;; Code:
+
+;; (setq centaur-logo nil)                        ; Logo file or nil (official logo)
+(setq centaur-full-name "JerryWang")           ; User full name
+(setq centaur-mail-address "1976883731@qq.com")   ; Email address
+;; (setq centaur-proxy "127.0.0.1:1080")          ; Network proxy
+;; (setq centaur-server nil)                      ; Enable `server-mode' or not: t or nil
+;; (setq centaur-icon nil)                        ; Display icons or not: t or nil
+(setq centaur-package-archives 'emacs-china)   ; Package repo: melpa, emacs-china, netease, ustc, tencent or tuna
+(setq centaur-theme 'default)                    ; Color theme: auto, random, default, classic, colorful, dark, light, day or night
+;; (setq centaur-dashboard nil)                   ; Use dashboard at startup or not: t or nil
+;; (setq centaur-restore-frame-geometry nil)      ; Restore the frame's geometry at startup: t or nil
+;; (setq centaur-lsp 'eglot)                      ; Set LSP client: lsp-mode, eglot or nil
+;; (setq centaur-lsp-format-on-save-ignore-modes '(c-mode c++-mode)) ; Ignore format on save for some languages
+(setq centaur-chinese-calendar t)              ; Use Chinese calendar or not: t or nil
+(setq centaur-prettify-symbols-alist nil)      ; Alist of symbol prettifications. Nil to use font supports ligatures.
+;; (setq centaur-prettify-org-symbols-alist nil)  ; Alist of symbol prettifications for `org-mode'
+;; (setq centaur-benchmark-init t)                ; Enable initialization benchmark or not: t or nil
+
+;; For Emacs devel
+;; (setq package-user-dir (locate-user-emacs-file (format "elpa-%s" emacs-major-version))r)
+;; (setq desktop-base-file-name (format ".emacs-%s.desktop" emacs-major-version))
+;; (setq desktop-base-lock-name (format ".emacs-%s.desktop.lock" emacs-major-version))
+
+;; Fonts
+(when (display-graphic-p)
+  ;; Set default font
+  (cl-loop for font in '("UniFont" "SauceCodePro" "SF Mono" "Hack" "Source Code Pro" "Fira Code"
+                         "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+           when (font-installed-p font)
+           return (set-face-attribute 'default nil
+                                      :font font
+                                      :height (cond (sys/mac-x-p 130)
+                                                    (sys/win32p 110)
+                                                    (t 120))))
+
+  ;; Specify font for all unicode characters
+  (cl-loop for font in '("UniFont" "Apple Color Emoji" "Symbola" "Symbol")
+           when (font-installed-p font)
+           return(set-fontset-font t 'unicode font nil 'prepend))
+
+  ;; Specify font for Chinese characters
+  (cl-loop for font in '("UniFont" "WenQuanYi Micro Hei" "Microsoft Yahei")
+           when (font-installed-p font)
+           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+
+;; Mail
+;; (setq message-send-mail-function 'smtpmail-send-it
+;;       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+;;       smtpmail-auth-credentials '(("smtp.gmail.com" 587
+;;                                    user-mail-address nil))
+;;       smtpmail-default-smtp-server "smtp.gmail.com"
+;;       smtpmail-smtp-server "smtp.gmail.com"
+;;       smtpmail-smtp-service 587)
+
+;; Calendar
+;; Set location , then press `S' can show the time of sunrise and sunset
+;; (setq calendar-location-name "Chengdu"
+;;       calendar-latitude 30.67
+;;       calendar-longitude 104.07)
+
+;; Misc.
+;; (setq confirm-kill-emacs 'y-or-n-p)
+
+;; Display on the specified monitor
+;; (when (and (> (length (display-monitor-attributes-list)) 1)
+;;            (> (display-pixel-width) 1920))
+;;   (set-frame-parameter nil 'left 1920))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; custom.el ends here
