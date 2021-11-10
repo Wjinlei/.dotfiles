f3d5491167983d5344c57c398f5faf7386528f78
Jerry Wang <1976883731@qq.com>
2021-11-10 21:54:00 +0800

    Update

--- xdg-define.sh
+++ xdg-define.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-export XDG_CACHE_HOME="$HOME/.userdata/cache"
-export XDG_CONFIG_HOME="$HOME/.userdata/config"
-export XDG_STATE_HOME="$HOME/.userdata/local"
 export XDG_DATA_HOME="$HOME/.userdata/data"
+export XDG_CONFIG_HOME="$HOME/.userdata/config"
+export XDG_CACHE_HOME="$HOME/.userdata/cache"
+export XDG_STATE_HOME="$HOME/.userdata/local"
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_TEMPLATES_DIR="$HOME/Templates"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_VIDEOS_DIR="$HOME/Videos"
