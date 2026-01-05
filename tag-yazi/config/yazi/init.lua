3823726fded125ddc58b53c28b553221c80629db
Jerry Wang <wjinlei.1996@qq.com>
2026-01-05 21:15:59 +0800

    chore(yazi): 更新yazi配置

--- tag-yazi/config/yazi/init.lua
+++ tag-yazi/config/yazi/init.lua
@@ -1,37 +1,25 @@
-function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
-	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = os.date("%b %d  %Y", time)
-	end
-
-	local size = self._file:size()
-	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
-end
-
-
 require("full-border"):setup {
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.PLAIN,
 }
 
-require("git"):setup()
-
-require("yaziline"):setup({
-  separator_style = "liney", -- "angly" | "curvy" | "liney" | "empty"
-  select_symbol = "",
-  yank_symbol = "󰆐",
-
-  filename_max_length = 24, -- truncate when filename > 24
-  filename_truncate_length = 6, -- leave 6 chars on both sides
-  filename_truncate_separator = "..."
-})
+th.git = th.git or {}
+th.git.deleted_sign = "D"
+th.git.modified_sign = "M"
+th.git.added_sign = "A"
+th.git.updated_sign = "U"
+th.git.untracked_sign = "?"
+th.git.ignored_sign = "X"
+require("git"):setup({})
 
 require("recycle-bin"):setup()
 
 require("bookmarks"):setup({
 	persist = "all",
+})
+
+require("yaziline"):setup({
+	separator_style = "liney", -- "angly" | "curvy" | "liney" | "empty"
+	select_symbol = "",
+	yank_symbol = "󰆐",
 })
